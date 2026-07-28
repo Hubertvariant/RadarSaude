9e82a1b2e8843fb73d9c968cb26beaa3090b1385
Hubertvariant <hubert.ramos@escola.pr.gov.br>
2026-07-28 18:44:19 -0300

    Rename BottomNavigation to SideNavigation and update tabs

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -1,6 +1,5 @@
 import { router, usePathname } from "expo-router";
 import {
-  House,
   ClipboardList,
   TriangleAlert,
   UserRound,
@@ -9,7 +8,7 @@ import { TouchableOpacity, View, Text } from "react-native";
 
 const tabs = [
   {
-    label: "Triagem",
+    label: "Unidades",
     icon: ClipboardList,
     route: "/atendimento",
   },
@@ -25,38 +24,43 @@ const tabs = [
   },
 ];
 
-export default function BottomNavigation() {
+export default function SideNavigation() {
   const pathname = usePathname();
 
   return (
-    <View className=" flex-row justify-around mt-2 bg-white py-4 shadow-xl">
-      {tabs.map((tab) => {
-        const Icon = tab.icon;
-        const active = pathname === tab.route;
+    <View className="w-20 h-full bg-white py-6 items-center border-r border-slate-100 shadow-md">
+      {/* Container das opções com espaçamento entre elas */}
+      <View className="gap-y-6 w-full items-center">
+        {tabs.map((tab) => {
+          const Icon = tab.icon;
+          const active = pathname === tab.route;
 
-        return (
-          <TouchableOpacity
-            key={tab.label}
-            className="items-center"
-            onPress={() => router.push(tab.route as any)}
-          >
-            <Icon
-              size={24}
-              color={active ? "#0284C7" : "#94A3B8"}
-            />
-
-            <Text
-              className={`mt-1 text-xs ${
-                active
-                  ? "font-semibold text-sky-600"
-                  : "text-slate-400"
+          return (
+            <TouchableOpacity
+              key={tab.label}
+              className={`items-center justify-center p-2 rounded-xl w-16 ${
+                active ? "bg-sky-50" : ""
               }`}
+              onPress={() => router.push(tab.route as any)}
             >
-              {tab.label}
-            </Text>
-          </TouchableOpacity>
-        );
-      })}
+              <Icon
+                size={24}
+                color={active ? "#0284C7" : "#94A3B8"}
+              />
+
+              <Text
+                className={`mt-1 text-[10px] text-center ${
+                  active
+                    ? "font-semibold text-sky-600"
+                    : "text-slate-400"
+                }`}
+              >
+                {tab.label}
+              </Text>
+            </TouchableOpacity>
+          );
+        })}
+      </View>
     </View>
   );
 }
